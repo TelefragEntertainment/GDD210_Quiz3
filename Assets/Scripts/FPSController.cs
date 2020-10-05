181956850b36b9d2ecd6e892c54f0b4610cddf1c
Blake <telefragent@gmail.com>
2020-10-05 15:37:25 -0400

    Initial commit

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+	public float MouseSensitivity;
+	public Transform CamTransform;
+
+	private float camRotation = 0f;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		float mouseInputY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+		float mouseInputX = Input.GetAxis("Mouse X") * MouseSensitivity;
+		camRotation -= mouseInputY;
+		camRotation = Mathf.Clamp(camRotation, -90f, 90f);
+		transform.localEulerAngles += new Vector3(-mouseInputY, mouseInputX, 0f);
+	}
+}
